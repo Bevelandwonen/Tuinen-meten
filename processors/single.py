7da6b218750f0ca4fe54688be2ae7559f7469aee
Thierry Ars <t.ars@bevelandwonen.nl>
2026-04-08 12:55:25 +0200

    changed param names

--- processors/single.py
+++ processors/single.py
@@ -2,25 +2,27 @@ from . import utils
 import geopandas as gpd
 from datatypes import DataBundle, HouseResult
 from shapely.geometry.polygon import Polygon
-from typing import Dict, List
+from typing import List
 
 def single_house(
     data: DataBundle, 
     perceel_poly: Polygon, 
-    gdf_bag_temp: gpd.GeoDataFrame,
-    gdf_weg_temp: gpd.GeoDataFrame,
+    gdf_bag_in_plot: gpd.GeoDataFrame,
+    gdf_road_in_plot: gpd.GeoDataFrame,
     visualise: bool = False
 ) -> List[HouseResult]:
-    
+
+    """Process a single house parcel and calculate garden size."""
+
     _, storage_size = utils.find_berging(
         perceel_poly, 
         data.gdf_pand,
     )
 
     garden_size = utils.calc_areas(
-        gdf_weg_temp, 
+        gdf_road_in_plot, 
         perceel_poly, 
-        gdf_bag_temp, 
+        gdf_bag_in_plot, 
         storage_size,
     )
 
@@ -28,17 +30,17 @@ def single_house(
         utils.visualise_house_perceel(
             data.gdf_perceel, 
             perceel_poly, 
-            gdf_bag_temp, 
-            gdf_weg_temp, 
+            gdf_bag_in_plot, 
+            gdf_road_in_plot, 
             storage_size,
         )
 
-    if gdf_bag_temp.empty:
+    if gdf_bag_in_plot.empty:
         raise ValueError("No BAG object found for single house")
 
     return [
         HouseResult(
-            pand_id=gdf_bag_temp.iloc[0]["identificatie"],
+            pand_id=gdf_bag_in_plot.iloc[0]["identificatie"],
             storage_size=storage_size,
             garden_size=garden_size,
             classification="single",
