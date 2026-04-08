bbb2c0b675a7f59be932e897d3ffd479c75c626d
Thierry Ars <t.ars@bevelandwonen.nl>
2026-04-08 12:41:40 +0200

    cleaned

--- processors/single.py
+++ processors/single.py
@@ -1,6 +1,6 @@
 from . import utils
 import geopandas as gpd
-from datatypes import DataBundle
+from datatypes import DataBundle, HouseResult
 from shapely.geometry.polygon import Polygon
 from typing import Dict, List
 
@@ -8,19 +8,39 @@ def single_house(
     data: DataBundle, 
     perceel_poly: Polygon, 
     gdf_bag_temp: gpd.GeoDataFrame,
-    gdf_perceel: gpd.GeoDataFrame,
-    gdf_weg_temp: gpd.GeoDataFrame
-) -> List[Dict]:
+    gdf_weg_temp: gpd.GeoDataFrame,
+    visualise: bool = False
+) -> List[HouseResult]:
     
-    _, storage_size = utils.find_berging(perceel_poly, data.gdf_pand)
-    garden_size = utils.calc_areas(gdf_weg_temp, perceel_poly, gdf_bag_temp, storage_size)
-    #TODO: Add option to visualise
-    #utils.visualise_house_perceel(gdf_perceel, perceel_poly, gdf_bag_temp, gdf_weg_temp, storage)
-    pand_id = gdf_bag_temp["identificatie"].values[0]
-    #TODO: what do we use as output
-    return [{
-        "Pand Id": pand_id,
-        "storage": storage_size,
-        "nieuw tuin opp": garden_size,
-        "classificatie": "single"
-    }]
+    _, storage_size = utils.find_berging(
+        perceel_poly, 
+        data.gdf_pand,
+    )
+
+    garden_size = utils.calc_areas(
+        gdf_weg_temp, 
+        perceel_poly, 
+        gdf_bag_temp, 
+        storage_size,
+    )
+
+    if visualise:
+        utils.visualise_house_perceel(
+            data.gdf_perceel, 
+            perceel_poly, 
+            gdf_bag_temp, 
+            gdf_weg_temp, 
+            storage_size,
+        )
+
+    if gdf_bag_temp.empty:
+        raise ValueError("No BAG object found for single house")
+
+    return [
+        HouseResult(
+            pand_id=gdf_bag_temp.iloc[0]["identificatie"],
+            storage_size=storage_size,
+            garden_size=garden_size,
+            classification="single",
+        )
+    ]
