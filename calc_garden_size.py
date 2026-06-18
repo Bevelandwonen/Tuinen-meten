5f4c4147859f8957a0f7fa2982b2e141fd046465
Thierry Ars <t.ars@bevelandwonen.nl>
2026-06-18 11:54:48 +0200

    cleaning up and adding constants

--- calc_garden_size.py
+++ calc_garden_size.py
@@ -19,6 +19,12 @@ warnings.filterwarnings('ignore', message='.*OGR.*')
 warnings.filterwarnings('ignore', message='.*Shapely.*')
 warnings.filterwarnings('ignore', message='.*Skipping field.*')
 
+PLOT_NUMBER = "Perceelnummer"
+PLOT_NUMBER_LEFT = "perceelLinks"
+PLOT_NUMBER_RIGHT = "perceelRechts"
+IDENTIFICATIE = "identificatie"
+BUILDING_ID = "Pand Id"
+
 if __name__ == "__main__": 
 
     #TODO: add new config options after restructuring
@@ -28,63 +34,48 @@ if __name__ == "__main__":
 
     data = utility.load_data(config, bbox)
 
-    ############## CHECK OF plot MATCHES ER AL ZIJN ##############
-    if "Perceelnummer" in data.df_units.columns and data.df_units["Perceelnummer"].notna().any():
+    if PLOT_NUMBER in data.df_units.columns and data.df_units[PLOT_NUMBER].notna().any():
         print("Found existing plot matches, skipping matching step.")
     else:
         print("No existing plot matches found, running matching step.")
 
-        plot_id = list(set(data.gdf_kad["perceelLinks"].tolist() + data.gdf_kad["perceelRechts"].tolist()))
-        print(f"Processing {len(plot_id)} plot numbers")
+        plot_id = (
+            pd.concat([
+                data.gdf_kad[PLOT_NUMBER_LEFT],
+                data.gdf_kad[PLOT_NUMBER_RIGHT],
+            ])
+            .dropna()
+            .unique()
+            .tolist()
+        )
 
-        # Check if an output file already exists if it does, check plot_nummers already found
-        if "Perceelnummer" in data.df_units.columns:
-            plot_id = (
-                pd.concat([
-                    data.gdf_kad["perceelLinks"],
-                    data.gdf_kad["perceelRechts"],
-                ])
-                .dropna()
-                .unique()
-                .tolist()
-            )
-            print(f"Found {len(plot_id)} new plot numbers to process")
-        
+        print(f"Found {len(plot_id)} new plot numbers to process")
+    
         df_units_plot_matches = utility.find_plot_id_per_unit(data.df_units, 
-                                                                       data.gdf_kad, 
-                                                                       data.gdf_bag, 
-                                                                       plot_id)
+                                                              data.gdf_kad, 
+                                                              data.gdf_bag, 
+                                                              plot_id)
 
         data.df_units = df_units_plot_matches   
-        data.df_units.to_excel(r"data/bag_ids/bag_ids_juiced.xlsx", index=False)
-        
-    ################################# Begin met berekenen tuinoppervlakte #################################
+        data.df_units.to_excel(r"data/bag_ids/bag_ids.xlsx", index=False)
 
-    # we only want buidings with a unique pand_id, we remove all rows without a unique pand_id
-    df_units_unique_pand = data.df_units[data.df_units["Pand Id"].duplicated(keep=False) == False]
-    df_units_filtered = df_units_unique_pand.dropna(subset=["Perceelnummer"])
-    # we do this to filter out flats etc.
+    # Calculate garden size 
+    # We remove buidings without a unique building_id, these are flats.
+    df_units_unique_building = data.df_units[data.df_units[BUILDING_ID].duplicated(keep=False)]
+    df_units_filtered = df_units_unique_building.dropna(subset=[PLOT_NUMBER])
 
     all_results = []
     errors = []
 
-    # we gaan elk plot langs
-    for plotnumber in df_units_filtered["Perceelnummer"].unique():
+    for plotnumber in df_units_filtered[PLOT_NUMBER].unique():
         print(f"Processing plot {plotnumber}")
-        #de eenheden in het plot
-        df_units_in_plot = df_units_filtered[df_units_filtered["Perceelnummer"] == plotnumber]
-        # verzameling van de lijnen van het plot
-        gdf_plot = data.gdf_kad[(data.gdf_kad["perceelLinks"] == plotnumber) | (data.gdf_kad["perceelRechts"] == plotnumber)]
-        # verzameling van de huizen in het plot
-        gdf_bag_in_plot = data.gdf_bag[data.gdf_bag["identificatie"].isin(df_units_in_plot["Pand Id"])]
-        # verzameling van de wegen in het plot
-        #TODO: shouldnt this be gdf_plot instead of gdf_bag_in_plot?
+        df_units_in_plot = df_units_filtered[df_units_filtered[PLOT_NUMBER] == plotnumber]
+        gdf_plot = data.gdf_kad[(data.gdf_kad[PLOT_NUMBER_LEFT] == plotnumber) |
+                                (data.gdf_kad[PLOT_NUMBER_RIGHT] == plotnumber)]
+        gdf_bag_in_plot = data.gdf_bag[data.gdf_bag[IDENTIFICATIE].isin(df_units_in_plot[BUILDING_ID])]
         gdf_road_in_plot = data.gdf_road[data.gdf_road.intersects(gdf_bag_in_plot.unary_union)]
-
-        #why dont we just give one  geometry?
-        #this because we can have multiple plots with the same number so we need to check again
-
-        #TODO: ugly code: maybe check validity of polygon if it failt continue
+        
+        #TODO: ugly code: maybe check validity of polygon if it failed continue
         try:
             plot_poly = utility.create_plot_polygon(gdf_plot["geometry"])
         except Exception as e:
@@ -92,33 +83,27 @@ if __name__ == "__main__":
             errors.append((plotnumber, str(e)))
             continue
             
-        # we do this so we can check create_plot_polygon is working correctly, if not we can check the error message
-        #TODO: Make this return an ENUM
         parcel_type = check_plot_type(df_units_in_plot, gdf_bag_in_plot)
-        #TODO: Catch results
         if parcel_type == PlotType.SINGLE:
             result = single_house(data, plot_poly, gdf_bag_in_plot, gdf_road_in_plot)
         elif parcel_type == PlotType.MULTIPLE_ALIGNED:
             result = multiple_aligned(data, plot_poly, gdf_bag_in_plot, gdf_plot, gdf_road_in_plot)
-        # TODO: USE ENUM HERE
         else:
             result = open_plot(data, plot_poly, gdf_bag_in_plot, gdf_plot, gdf_road_in_plot)
 
         all_results.extend(result)
     
-    # concatenate the results with data.df_units based on pand_id
     df_results = pd.DataFrame([r.__dict__ for r in all_results])
-    df_results.rename(columns={"pand_id": "Pand Id"}, inplace=True)
-    df_eenheid_nieuw = pd.merge(data.df_units, df_results, left_on="Pand Id", right_on="Pand Id", how="left")
-    # For every row with a non unique pand_id set classification to "non unique pand_id, prob flat"
-    #TODO: clenaup this ugly code
-    for plotnumber, error in errors:
-        df_eenheid_nieuw.loc[df_eenheid_nieuw["Perceelnummer"] == plotnumber, "class"] = f"error in plot polygon creation: {error}"
-
-    df_eenheid_nieuw.loc[data.df_units["Pand Id"].duplicated(keep=False), "class"] = "non unique pand_id, prob flat"
+    df_results.rename(columns={"pand_id": BUILDING_ID}, inplace=True)
+    df_units_results = pd.merge(data.df_units, df_results, left_on=BUILDING_ID, right_on=BUILDING_ID, how="left")
     
+    for plotnumber, error in errors:
+        df_units_results.loc[df_units_results[PLOT_NUMBER] == plotnumber,
+                             "class"] = f"error in plot polygon creation: {error}"
 
+    df_units_results.loc[data.df_units[BUILDING_ID].duplicated(keep=False),
+                         "class"] = "non unique building_id, prob flat"
+    
     #TODO: ADD OUTLIER CHECK PER PARCEL
 
-
-    df_eenheid_nieuw.to_excel("garden_size_nieuw_eerste_run.xlsx")
+    df_units_results.to_excel("garden_size_nieuw_eerste_run.xlsx")
